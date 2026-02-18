814799105d8e2df6241ddc29801b185eda5a0cae
Pulkit Kulshreshtha <pulkit.kulshreshtha_cs.aiml24@gla.ac.in>
2026-02-18 15:12:53 +0530

    Add pin definitions for ultrasonic sensor

--- src/main.cpp
+++ src/main.cpp
@@ -14,3 +14,10 @@
  * @author Pulkit Kulshreshtha
  * @date 18 Feb 2026
  */
+
+// Pin Definitions
+int trigPin = 9;   ///< Trigger pin connected to digital pin 9
+int echoPin = 10;  ///< Echo pin connected to digital pin 10
+
+long duration;     ///< Stores the time taken for echo to return (in microseconds)
+float distance;    ///< Stores calculated distance in centimeters
