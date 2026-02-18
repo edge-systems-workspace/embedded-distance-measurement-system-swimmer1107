53094d062e8b2f5b90a88bab0384f78b815e926e
Pulkit Kulshreshtha <pulkit.kulshreshtha_cs.aiml24@gla.ac.in>
2026-02-18 15:12:28 +0530

    Revise header and comments for ultrasonic sensor code
    Updated file header and added detailed comments about the ultrasonic distance measurement program.

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,16 @@
-#include <Arduino.h>
 /**
- * @file main.cpp
- * @brief Embedded Distance Measurement using Ultrasonic Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @file Ultrasonic_Distance_Measurement.ino
+ * @brief Distance measurement using HC-SR04 Ultrasonic Sensor.
  *
- * @details
- * Measures distance using HC-SR04 ultrasonic sensor
- * and displays structured output via Serial Monitor.
+ * This program measures the distance of an object using an
+ * ultrasonic sensor. It sends a 10µs trigger pulse, receives
+ * the echo signal, calculates the distance in centimeters,
+ * and prints the result to the Serial Monitor.
+ *
+ * Formula Used:
+ * Distance (cm) = (Time × Speed of Sound) / 2
+ * Speed of Sound ≈ 0.034 cm/µs
+ *
+ * @author Pulkit Kulshreshtha
+ * @date 18 Feb 2026
  */
-
- // TODO 1:
- // Define TRIG pin (Use pin 9)
-
- // TODO 2:
- // Define ECHO pin (Use pin 10)
-
- // TODO 3:
- // Create variable to store duration
-
- // TODO 4:
- // Create variable to store calculated distance
-
-void setup() {
-
-    // TODO 5:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 6:
-    // Configure TRIG as OUTPUT
-
-    // TODO 7:
-    // Configure ECHO as INPUT
-
-    // TODO 8:
-    // Print system initialization message
-}
-
-void loop() {
-
-    // TODO 9:
-    // Set TRIG LOW for 2 microseconds
-
-    // TODO 10:
-    // Send 10 microsecond pulse on TRIG
-
-    // TODO 11:
-    // Measure pulse duration on ECHO using pulseIn()
-
-    // TODO 12:
-    // Calculate distance in cm
-
-    // TODO 13:
-    // Print calculated distance
-
-    // TODO 14:
-    // Add delay (500ms)
-}
